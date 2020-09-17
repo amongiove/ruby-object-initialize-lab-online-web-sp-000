141c473da3d519db4d473805dd3875b014ad4b24
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-17 21:37:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,12 @@ class Dog
     @name = name
   end
 
-  def name=(name)
-    @name = name
+  def name
+    @name
   end
 
-  def breed=(breed)
-    @breed = breed
+  def breed
+    @breed
   end
 
 
